7651839c0538d2145be28d8c5bb514c799775af6
guinegreiros-dev <guilhermenegreiros12@gmail.com>
2024-10-01 16:45:36 -0300

    remove rms

--- sg.tf
+++ sg.tf
@@ -3,16 +3,14 @@ resource "aws_security_group" "sg" {
   description = "fiap-group-23"
   vpc_id      = data.aws_vpc.vpc.id
 
-  ingress {
-    description = "HTTP Restaurant Port"
-    from_port   = 30001
-    to_port     = 30001
-    protocol    = "tcp"
+ ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    description = "All"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
